bce7c3221b6bd2d2758b4f3611c2bb70ea14c066
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2023-10-17 01:33:15 +0000

    16.10

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador<listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
     }
 
-document.querySelector(".tecla_risada").onclick = tocaSomRisada;
+    tecla.onkeydown = function(evento){
+        console.log(evento.code)
+
+        if(evento.code == 'Space'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
